b25c6d0af75547296ab127ece2c0676ff327af4a
GOPINISAIBHUVAN <gopinisaibhuvan@gmail.com>
2022-06-27 13:37:50 +0530

    practice sheet 3 completed

--- src/jav_practice_sheet_3.java
+++ src/jav_practice_sheet_3.java
@@ -1,14 +1,11 @@
 import java.util.*;
-
 public class jav_practice_sheet_3 {
     public static void main(String[] args) {
          //problem 1
-
         String name = "JAVACODER";
         System.out.println(name.toLowerCase());
 
         //problem 2
-
         String replace = " hi my name is java coder";
         System.out.println(replace.replace(" ","_"));
 
@@ -17,9 +14,15 @@ public class jav_practice_sheet_3 {
         Scanner s = new Scanner(System.in);
         String Name =s.nextLine();
         System.out.printf(" \" Dear %s , Thanks a lot \" ",Name);
+        System.out.println();
 
         //problem 4
+        String detect = " in this sentence we have  double and   triple spaces";
+        System.out.println(detect.indexOf("  "));
+        System.out.println(detect.indexOf("   "));
 
+        //problem 5
+        System.out.println("\"Dear javacoder, This\tjava\tinformation\tis\tnice.Thanks\"");
 
     }
 }
