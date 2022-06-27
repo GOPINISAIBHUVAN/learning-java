4bba3685bef098096b1c640fbc92d66188267ca5
GOPINISAIBHUVAN <gopinisaibhuvan@gmail.com>
2022-06-27 12:34:17 +0530

    started practice sheet 3

---
+++ src/jav_practice_sheet_3.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class jav_practice_sheet_3 {
+    public static void main(String[] args) {
+         //problem 1
+
+        String name = "JAVACODER";
+        System.out.println(name.toLowerCase());
+
+        //problem 2
+
+        String replace = " hi my name is java coder";
+        System.out.println(replace.replace(" ","_"));
+
+        //problem 3
+        System.out.println("Enter Name ");
+        Scanner s = new Scanner(System.in);
+        String Name =s.nextLine();
+        System.out.printf(" \" Dear %s , Thanks a lot \" ",Name);
+
+        //problem 4
+
+
+    }
+}
