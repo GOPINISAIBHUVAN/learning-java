44aebc8ca230ecb89a0c7fe328158886057a4451
GOPINISAIBHUVAN <gopinisaibhuvan@gmail.com>
2022-07-01 12:39:38 +0530

    completed 6 problrm in practice sheet 5

--- src/java_practice_sheet_5.java
+++ src/java_practice_sheet_5.java
@@ -17,7 +17,45 @@ public class java_practice_sheet_5 {
         }
 
         // problem 2
+        int sum = 0;
+        int i =1;
+        int n=3;
+        while(i<=n){
+            sum = sum +(2*i);
+            i++;
+        }
+        System.out.println(sum);
 
+        //problem 3
+        int n1 = 7;
+        for(int i1=1;i1<=10;i1++){
+            System.out.println(n1+"*"+i1+"="+(n1*i1));
+        }
+
+        // problem 4
+        int n2 =10;
+        for(int i2=10;i2>=1;i2--){
+            System.out.printf("%d * %d = %d",n2,i2,n2*i2);
+            System.out.println(" ");
+        }
+
+        // problem 5
+          int multi = 1;
+        int n3 = 4;
+        while(n3>0){
+            multi = multi * n3;
+            n3--;
+        }
+        System.out.println(multi);
+
+        // problem 6
+        int multi1=1;
+        for(int n4 =6;n4>0;n4--){
+            multi1= multi1 * n4;
+        }
+        System.out.println(multi1);
+
+        // problem 7
 
     }
 }
