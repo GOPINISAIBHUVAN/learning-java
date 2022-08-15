d9694561190f65ec477bf8c411bb4d82551d4846
GOPINISAIBHUVAN <gopinisaibhuvan@gmail.com>
2022-08-15 15:41:57 +0530

    completed exercise 3

--- src/exercise_3.java
+++ src/exercise_3.java
@@ -1,8 +1,53 @@
+import java.util.Random;
+import java.util.*;
 class game {
+    public int number;
+    public int inputNumber;
+    public int numberOfGuesses = 0;
 
+    public void setNumberOfGuesses(int numberOfGuesses) {
+
+        this.numberOfGuesses = numberOfGuesses;
+    }
+
+    public int getNumberOfGuesses() {
+
+        return numberOfGuesses;
+    }
+
+    game(){
+       System.out.println("hi..welcome to guess number game..!!");
+    Random rand = new Random() ;
+    this.number = rand.nextInt(100);
+}
+void takeUserInput(){
+Scanner sc = new Scanner(System.in);
+    System.out.println("Enter number:");
+this.inputNumber = sc.nextInt();
+
+}
+boolean isCorrectNumber() {
+        numberOfGuesses++;
+    if (inputNumber == number) {
+        System.out.format("you and computer guess the same value.the value is %d and u find the value in this number of %d times",number,numberOfGuesses);
+        return true;
+    }
+    else if (inputNumber > number) {
+        System.out.println("your number is too high");
+    }
+    else if (inputNumber < number) {
+        System.out.println("your number is too low");
+    }
+    return false;
+}
 }
 public class exercise_3 {
     public static void main(String[] args) {
-
+game sc = new game();
+       boolean s = false ;
+while(!s){
+sc.takeUserInput();
+s= sc.isCorrectNumber();
+    }
     }
 }
--- src/inheritance.java
+++ src/inheritance.java
@@ -12,4 +12,5 @@ public class inheritance {
     public static void main(String[] args) {
 
     }
+
 }
