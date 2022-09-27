f920f1c63580f2d6ed8dd4c318f3aa63aa5a86df
GOPINI SAI BHUVAN <gopinisaibhuvan@gmail.com>
2022-09-28 03:56:55 +0530

    completed adj_co_prime_arr

--- src/adj_co_prime_arr.java
+++ src/adj_co_prime_arr.java
@@ -1,38 +1,36 @@
 import java.util.*;
-class co_prime{
-    void adj_co_prime(int[]arr ){
-        int x;
+class arr_co_prime{
+    void adj_co_prime(int[] arr ){
+        int x = 0;
         for(int i=0;i<arr.length-1;i++) {
-
             int max = 1;
-            if(arr[i]==arr[i+1]){
-                System.out.println(arr[i]);
+            if(arr[i]== arr[i+1]){
+                max = arr[i];
+            }
+            if(arr[i]==0 && arr[i+1]==0) {
+                x = 0;
             }
             if(arr[i]>arr[i+1]) {
                 max = arr[i];
             }
-            if(arr[i]>arr[i+1]){
+            if(arr[i]<arr[i+1]){
                 max = arr[i+1];
             }
-
             for(int j=1;j<=max;j++) {
                 if (arr[i] % j == 0 && arr[i + 1] % j == 0) {
                     x = j;
-                } else {
-                    x = j;
-                }
-                if (x == 1){
-                    System.out.println("the numbers are co prime");
-                }
-                else{
-                    System.out.println("the numbers are not co prime");
                 }
             }
-
+            if (x == 1){
+                System.out.println("the numbers are co prime");
+            }
+            else{
+                System.out.println("the numbers are not co prime");
+            }
         }
     }
 }
-public class krishna_anna_code {
+public class adj_co_prime_arr {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter array length :");
@@ -42,7 +40,7 @@ public class krishna_anna_code {
             System.out.println("Enter array values" +  i );
             arr[i] = sc.nextInt();
         }
-co_prime obj = new co_prime();
+arr_co_prime obj = new arr_co_prime();
 obj.adj_co_prime(arr);
     }
 }
